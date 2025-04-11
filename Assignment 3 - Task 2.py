13a983c8cd160cb85c371309fe3e6fa40d2ef615
Shabad2507 <86426156+Shabad2507@users.noreply.github.com>
2025-04-11 19:02:36 +0530

    Add files via upload

---
+++ Assignment 3 - Task 2.py
@@ -0,0 +1,8 @@
+import math
+x = int(input("Enter a number: "))
+square_root = pow(x, 1/2)
+log = math.log(x)
+sine = math.sin(x)
+print("Square root: " + str(square_root))
+print("Logarithm: " + str(log))
+print("Sine: " + str(sine))
